910aa5adea1e479cc6a7322b15cf45feadf724b1
josephjreije35 <josephjreije35@gmail.com>
2023-01-27 10:36:21 +0100

    c

--- facts.py
+++ facts.py
@@ -9,9 +9,15 @@ people = [
            facts=['Fact-right.', 'Fact-write.', 'Fact-right.']),
         Person(github_login='javanochka', first_name='Anna', last_name='Nikiforovskaja',
            facts=['I can meow like a cat', 'I am a PhD student', 'I am not Estonian']),
+<<<<<<< HEAD
+    Person(github_login='srunnalin', first_name='Nalin', last_name='Srun',
+           facts=['I like to sleep in my freetime :)', 'I am a master student', 'I am from Cambodia']),
+]
+=======
     Person(github_login='zouzou135', first_name='Joseph', 
 last_name='Joseph',
            facts=['I am a slow reader', 'I am a Master student', 'I play the saxophone']),]
+>>>>>>> 6a5cabc (c)
 #facts are numerated from 0
 answers = {'example': 1,
            'javanochka': 2,
@@ -78,5 +84,5 @@ if __name__ == '__main__':
         print('> ', end='', flush=True)
         try:
             run_cmd(sys.stdin.readline().strip())
-        except Exception: # This is a VERY bad style. 
+        except Exception: # This is a VERY bad style.
             continue
